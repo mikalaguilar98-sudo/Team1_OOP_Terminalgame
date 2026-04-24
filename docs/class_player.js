63d9cb3c5d88782d7d7f0fab051c2667729f616c
mikalaguilar98-sudo <mikalaguilar98@gmail.com>
2026-04-24 16:26:58 -0600

    Add files via upload

---
+++ docs/class_player.js
@@ -0,0 +1,12 @@
+var class_player =
+[
+    [ "Player", "class_player.html#a1114770ebd2eae368e6c8810755a010c", null ],
+    [ "attackEnemy", "class_player.html#a5490821d6ff31034029177ba9c075aaa", null ],
+    [ "getDamage", "class_player.html#a3c0242a0a486e9548ebae8dad82593d7", null ],
+    [ "getScore", "class_player.html#acbdb2bae8d732157af0ca5f5438a2b63", null ],
+    [ "getWeaponX", "class_player.html#a37e3ef0926b9e7c28d0dcab86b72365e", null ],
+    [ "getWeaponY", "class_player.html#ad53eef3d9f43c04036ff43bdcddbd2af", null ],
+    [ "move", "class_player.html#aebb43c51a7da9e652f5574e8ded62f87", null ],
+    [ "Score", "class_player.html#a1dfe0ea1f7cd05f54087d19e24da72f2", null ],
+    [ "setDamage", "class_player.html#a3bae46eff7f6f1e2c37476523c832384", null ]
+];
---
+++ docs/class_world.js
@@ -0,0 +1,11 @@
+var class_world =
+[
+    [ "World", "class_world.html#a871b25c543df03c50eb23462fad01761", null ],
+    [ "addenemy", "class_world.html#ad469b8d0691ec06f094fff0bec71c7c9", null ],
+    [ "gameLoop", "class_world.html#af2b4ba4ce188c2770f13108e4277414f", null ],
+    [ "getEnemies", "class_world.html#a85f9a8f84cf1b8488c19d3e984e33a8e", null ],
+    [ "getMutex", "class_world.html#a7429d5877b0c79326da6a88758272699", null ],
+    [ "getPlayer", "class_world.html#ad992171c7b88c0f34449033a9fe22481", null ],
+    [ "moveEnemies", "class_world.html#aa27e80a41c7e0dbd24af1e1cbf3898ee", null ],
+    [ "movePlayer", "class_world.html#a1daaaadcd0afa4da853da23bb970d10c", null ]
+];
