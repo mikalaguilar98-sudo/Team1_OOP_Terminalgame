89551d7d611de7699aaf2d866d167606de331cbc
mikalaguilar98-sudo <mikalaguilar98@gmail.com>
2026-04-24 16:07:00 -0600

    Add files via upload

---
+++ class_entity.js
@@ -0,0 +1,9 @@
+var class_entity =
+[
+    [ "Entity", "class_entity.html#a9f6d965fe34c1975599d431783b6f2c6", null ],
+    [ "getHealthPoints", "class_entity.html#a869c6d29311a34dfa0218ecbd2d34fda", null ],
+    [ "getX", "class_entity.html#a5a851d21579432e736fb7c479e86fb6b", null ],
+    [ "getY", "class_entity.html#ae48a05fef6420bb320d17aed7ea98cb3", null ],
+    [ "move", "class_entity.html#abd3c1da61bfefb942f609e5f8606f1cc", null ],
+    [ "takeDamage", "class_entity.html#a9534c576045aabcf406d1f891d5b3b60", null ]
+];
---
+++ class_game.js
@@ -0,0 +1,5 @@
+var class_game =
+[
+    [ "Game", "class_game.html#a4e9cc7d2d517cafdb36395c465cbabe7", null ],
+    [ "startGame", "class_game.html#ae8638ccdb0ef3bf39a6affa30aa1258f", null ]
+];
